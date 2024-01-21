cacfa23c5f20e1f9aafb86221b58d1c65d4981ac
Michael Papesca <michael.papesca@onmail.com>
2024-01-21 00:16:51 -0700

    add linter and fix tests

--- App.test.tsx
+++ App.test.tsx
@@ -5,8 +5,7 @@ import App from './App';
 
 describe('<App />', () => {
   it('has 1 child', () => {
-    // const tree = renderer.create(<App />).toJSON() as renderer.ReactTestRendererJSON;
-    // expect(tree.children?.length ?? 0).toBe(1);
-    expect(0).toBe(0);
+    const tree = renderer.create(<App />).toJSON() as renderer.ReactTestRendererJSON;
+    expect(tree.children?.length).toBe(2);
   });
 });
